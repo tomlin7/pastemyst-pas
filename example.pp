b5402071a956a58d36880f526a288bf948662db7
billyeatcookies <billydevbusiness@gmail.com>
2021-01-12 19:39:45 +0530

    added examples

---
+++ example.pp
@@ -0,0 +1,34 @@
+{Example - Getting a Paste }
+
+Program example;
+
+{--------------------------------------------------------------}
+{ import the pastemyst unit to get started }
+
+uses pastemyst;
+
+{--------------------------------------------------------------}
+{ Variable Declarations}
+
+var paste_id: string;
+
+{--------------------------------------------------------------}
+{ Prints Output to The console }
+
+procedure print(s: string);
+begin
+   Write(s, ^M);
+end;
+
+
+{--------------------------------------------------------------}
+{ Main Program }
+
+begin
+   Read(paste_id);
+   writeln('Fetching Paste with Id: ', paste_id);
+   print(get_paste(paste_id));
+end.
+
+
+{--------------------------------------------------------------}
