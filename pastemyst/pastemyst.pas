046a28b16ac6f921f1f7b67749e14db5a6592607
billyeatcookies <billydevbusiness@gmail.com>
2021-01-21 10:09:45 +0100

    ✨ check user function

--- pastemyst/pastemyst.pas
+++ pastemyst/pastemyst.pas
@@ -24,6 +24,16 @@ function get_paste(_id: string): string;
 function get_private_paste(_id: string; _token: string): string;
 
 {--------------------------------------------------------------}
+{ Creating a new public paste - Interface }
+
+// function create_paste(params: string): string;
+
+{ checks user - interface }
+
+function check_user(user_id: string): string;
+
+
+{--------------------------------------------------------------}
 { Private Implementation }
 
 implementation
@@ -38,7 +48,7 @@ uses
 {--------------------------------------------------------------}
 { Constant Declarations }
 
-const url = 'https://paste.myst.rs/api/v2/paste/';
+const url = 'https://paste.myst.rs/api/v2/';
 
 {--------------------------------------------------------------}
 { Variable Declarations }
@@ -52,7 +62,7 @@ function get_paste(_id: string): string;
 begin
    Client := TFPHttpClient.Create(Nil);
    try
-      get_paste := Client.Get(url + _id);
+      get_paste := Client.Get(url + 'paste/' + _id);
    finally
       Client.Free;
    end;
@@ -67,12 +77,51 @@ begin
    Client := TFPHttpClient.Create(Nil);
    Client.AddHeader('Authorization', _token);
    try
-      get_private_paste := Client.Get(url + _id);
+      get_private_paste := Client.Get(url + 'paste/' + _id);
    finally
       Client.Free;
    end;
 end;
-Pcontr
+
+
+{--------------------------------------------------------------}
+{ Creating a new public paste - Implementation }
+{
+function create_paste(params: string): string;
+
+var Response : TStringStream;
+
+begin
+   Client := TFPHttpClient.Create(Nil);
+   Client.RequestBody := TRawByteStringStream.Create(params);
+   Response := TStringStream.Create('');
+   try
+      try
+         Client.Post(url, Response);
+         create_paste := ('Response code is ' + inttostr(Client.ResponseStatusCode));
+      except on E: Exception do
+         create_paste := ('Somethign bad happened: ' + E.message);
+      end;
+   finally
+      Client.RequestedBody.Free;
+      Client.Free;
+      Response.Free;
+   end;
+end;
+}
+
+function check_user(user_id: string): string;
+begin
+   Client := TFPHttpClient.Create(Nil);
+   try
+      check_user := Client.Get(url + 'user/' + user_id);
+   finally
+      Client.Free;
+   end;
+end;
 {--------------------------------------------------------------}
 
 end.
+
+
+function ew();
