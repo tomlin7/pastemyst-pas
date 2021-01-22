664233e3e7a681f844b3ee17feae7048f23d8a4e
billyeatcookies <billydevbusiness@gmail.com>
2021-01-22 06:02:17 +0100

    ✨ Added user_exists example

---
+++ examples/example_8.pp
@@ -0,0 +1,24 @@
+{Example -  }
+
+Program example;
+
+{--------------------------------------------------------------}
+{ import the pastemyst unit to get started }
+
+uses pastemyst in '../pastemyst/pastemyst.pas';
+
+{--------------------------------------------------------------}
+{ Variable Declarations}
+
+var user_name: string;
+
+{--------------------------------------------------------------}
+{ Main Program }
+
+begin
+   readln(user_name);
+   writeln(user_exists(user_name));
+end.
+
+
+{--------------------------------------------------------------}
